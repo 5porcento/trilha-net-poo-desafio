b8e776f26e14355622c0dba3559677a4fc6a4e62
Kauan Pacheco <kauanpacheco.dev@gmail.com>
2024-02-15 13:46:37 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+System.Console.WriteLine("Smatphone Nokia:");
+Smartphone nokia = new Nokia(numero:"1234",modelo:"modelo1",imei:"22344",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("ZAP-ZAP");
+
+System.Console.WriteLine("Smatphone Iphone:");
+Smartphone iphone = new Iphone(numero:"45678",modelo:"12 pro max",imei:"1111111",256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
